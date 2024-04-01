58a6c02c704e8284457b35730519ff6ac9ae7e0b
prashunbaral <P.Baral2@wlv.ac.uk>
2024-04-01 09:54:08 +0545

    updated RaceTrackMission script

--- games/Assets/RaceMissiontrigger.cs
+++ games/Assets/RaceMissiontrigger.cs
@@ -1,16 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using TMPro.Examples;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class RaceMissiontrigger : MonoBehaviour
 {
     public GameObject MissionStart;
+    public GameObject RaceMissionUI;
+    public GameObject Lap1;
+    public GameObject Lap2;
+    public GameObject Lap3;
+    public TextMeshPro TimerUI;
+    public TextMeshProUGUI ScoreUI;
 
+
+    private void Start()
+    {
+        RaceMissionUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == MissionStart)
         {
-            Debug.Log("Hello! The MissionStart GameObject entered the trigger.");
+            
+            RaceMissionUI.SetActive(true);
+            StartCoroutine(DeactivateMission3UI());
+
+            IEnumerator DeactivateMission3UI()
+            {
+                yield return new WaitForSeconds(3f);
+                RaceMissionUI.SetActive(false);
+                
+            }
+
+
+
+           
         }
     }
 }
